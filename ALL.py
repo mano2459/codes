ba26cecc392dfa119cdfce7e7b24d63b2fa51dce
mano2459 <94215500+mano2459@users.noreply.github.com>
2021-11-18 12:57:22 +0530

    Update ALL.py

--- ALL.py
+++ ALL.py
@@ -151,23 +151,23 @@ for i in range (a):
     print (d)'''
 
 
-'''#11WAP A PROGRAM TO INPUT THE NAME OF N COUNTRIES AND THEIR CAPITAL,CURRENCY AND DISPLAY IN A TABULAR FORM.
-d={}
-e=int(input("Enter the number of counrties:"))
-i=1
-while i<=e:
-    co=input("enter the country name:")
-    cap=input("Enter the capital name :")
-    curre=input("enter the currency name:")
-    d[co]=[cap,curre]
-    i+=1
-l=d.keys()
-print("\ncountry\t\t","capital\t","currency")
+'''# 11WAP A PROGRAM TO INPUT THE NAME OF N COUNTRIES AND
+#  THEIR CAPITAL,CURRENCY AND DISPLAY IN A TABULAR FORM.
+d = {}
+e = int(input("Enter the number of counrties:"))
+i = 1
+while i <= e:
+    co = input("enter the country name:")
+    cap = input("Enter the capital name :")
+    curre = input("enter the currency name:")
+    d[co] = [cap, curre]
+    i += 1
+l = d.keys()
+print("\ncountry\t\t", "capital\t", "currency")
 for i in l:
-   z=d[i]
-   print(i,"\t\t",end=" ")
-   for j in z:
-       print (j,"\t\t",end=" ")'''
+    z = d[i]
+    print(i, "\t\t", d[i][0], d[i][1])
+'''
 
 
 
